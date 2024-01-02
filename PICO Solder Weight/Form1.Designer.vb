5b8a004e307c1b1b104f5ece4603a3376baf9439
Grantjsc <gjcatapang10@gmail.com>
2024-01-02 15:57:23 +0800

    Changing COM8 to COM3

--- PICO Solder Weight/Form1.Designer.vb
+++ PICO Solder Weight/Form1.Designer.vb
@@ -706,7 +706,7 @@ Partial Class Form1
         '
         'SerialPort1
         '
-        Me.SerialPort1.PortName = "COM8"
+        Me.SerialPort1.PortName = "COM3"
         '
         'Timer1
         '
